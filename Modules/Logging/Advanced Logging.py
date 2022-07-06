be958916476ed819204f48a3b4195fc27eda27a8
Kassam Dakhlalah <96330526+Kassam-99@users.noreply.github.com>
2022-07-06 15:14:12 +0000

    Update Advanced Logging.py

--- Modules/Logging/Advanced Logging.py
+++ Modules/Logging/Advanced Logging.py
@@ -3,15 +3,20 @@ import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.WARNING)
 
+
 formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s')
 file_handler = logging.FileHandler("sample.log")
 file_handler.setFormatter(formatter)
 file_handler.setLevel(logging.ERROR)
 
+
 logger.addHandler(file_handler)
 
 
-logger.debug('This message should go to the log file')
-logger.info('So should this')
-logger.warning('And this, too')
-logger.error("And non-ASCII stuff, too, like Øresund and Malmö")
+
+logging.debug("It is used to provide detailed information and only use it when there is diagnosing problems.")
+logging.info("It provides the information regarding that things are working as we want.")
+logging.warning("It is used to warn that something happened unexpectedly, or we will face the problem in the upcoming time")
+logging.error("It is used to inform when we are in some serious trouble, the software hasn't executed some programs.")
+logging.critical("It specifies the serious error, the program itself may be incapable of remaining executing.")
+
